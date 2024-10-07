5bf9087bb5d8faa2e140da8f891cfd4b35267c63
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-10-07 12:27:57 -0500

    Update interfaz_ETAS.py

--- interfaz_ETAS.py
+++ interfaz_ETAS.py
@@ -133,7 +133,7 @@ def main_view():
     if st.button("+ Agregar otra entrada"):
         st.session_state.container_entries += 1
     # Botón para enviar los datos ingresados
-    if st.button("ENVIAR"):
+    if st.button("ENVIAR", key="send_button"):
         container_data = []
         for i in range(st.session_state.container_entries):
             container_data.append({
