3802b2f1090b3f4ff5e2395bce1c54acb109e615
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-10-07 15:10:54 -0500

    Update interfaz_ETAS.py

--- interfaz_ETAS.py
+++ interfaz_ETAS.py
@@ -11,8 +11,8 @@ def get_db_connection():
             dbname="consultaETAS",
             user="postgres",
             password="Daniel2030#",
-            host="6.tcp.ngrok.io",
-            port="15745"
+            host="0.tcp.ngrok.io",
+            port="19644"
         )
         return conn
     except psycopg2.Error as e:
