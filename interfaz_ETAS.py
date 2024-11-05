f6556d8d5ed8ddcbdc5b71347559c90cabcc1749
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-11-05 08:51:47 -0500

    Update interfaz_ETAS.py

--- interfaz_ETAS.py
+++ interfaz_ETAS.py
@@ -134,7 +134,7 @@ def register_or_login_view():
                     st.session_state['id'] = user_id
                     st.session_state['email'] = email
                     st.success("Inicio de sesión exitoso")
-                    st.experimental_rerun()
+                    st.experimental.rerun()
                 else:
                     st.error("Usuario o contraseña incorrectos")
             else:
