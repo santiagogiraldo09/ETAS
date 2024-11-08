9d7380a9aa158525b388f8e1af048f38e20e58b8
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2024-11-08 11:24:49 -0500

    Update interfaz_ETAS.py

--- interfaz_ETAS.py
+++ interfaz_ETAS.py
@@ -139,11 +139,11 @@ def history_view():
                     col1, col2, col3 = st.columns(3)
     
                     with col1:
-                        st.text_input("Número de Contenedor", value=num_contenedor, key=f'num_contenedor_{idx}', disabled=True)
+                        st.metric(label="Número de Contenedor", value=num_contenedor)
                     with col2:
-                        st.text_input("Naviera", value=naviera, key=f'naviera_{idx}', disabled=True)
+                        st.metric(label="Naviera", value=naviera)
                     with col3:
-                        st.text_input("ETA", value=eta, key=f'eta_{idx}', disabled=False)
+                        st.metric(label="ETA", value=eta)
     
                     st.markdown("---")  # Línea de separación entre registros
                 
